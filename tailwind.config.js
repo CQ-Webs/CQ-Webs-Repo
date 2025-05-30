de7145517ae7ba149ff8d88d9eb205fbfa5bd804
CQ-Webs <your.email@example.com>
2025-05-29 20:22:42 -0600

    fixing tailwindcss #1

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",       // For Next.js
+    "./components/**/*.{js,ts,jsx,tsx}",  // Adjust if you have a different folder structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
